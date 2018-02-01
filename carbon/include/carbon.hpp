24c70e546f4fe42fab7a54d2b90fe03677cb0bfe
Justas Masiulis <justas.masiulis@yahoo.com>
2018-02-01 21:24:45 +0200

    initial commit

---
+++ carbon/include/carbon.hpp
@@ -0,0 +1,58 @@
+/*
+* Copyright 2018 Justas Masiulis
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#pragma once
+#include "carbon/detail/generated_macros.hpp"
+#include "carbon/detail/nargs_macro.hpp"
+
+// needed to fix msvc va args expansion
+#define CRBN_DETAIL_EXPAND(...) __VA_ARGS__
+
+#define CARBON_SERIALIZABLE_(...) \
+    CRBN_DETAIL_CAT(CRBN_DETAIL_SERIALIZABLE_, CRBN_DETAIL_NARGS(__VA_ARGS__))
+
+#define CARBON_SERIALIZABLE(class_name, ...) \
+    using serializer_type =                  \
+        CRBN_DETAIL_EXPAND(CARBON_SERIALIZABLE_(__VA_ARGS__)(class_name, __VA_ARGS__))
+
+namespace carbon {
+
+    namespace detail {
+
+        template<class>
+        struct member_traits;
+
+        template<class R, class T>
+        struct member_traits<R T::*> {
+            using value_type = R;
+            using class_type = T;
+        };
+
+        template<class T>
+        using member_class_t = typename member_traits<T>::class_type;
+
+        template<class T>
+        using member_value_t = typename member_traits<T>::value_type;
+    } // namespace detail
+
+
+    template<class T, detail::member_value_t<T> detail::member_class_t<T>::*P>
+    struct m_ {};
+
+    template<class... Ms>
+    struct serializer {};
+
+} // namespace carbon
---
+++ carbon/test/main.cpp
@@ -0,0 +1,14 @@
+#if 1
+
+#include "../include/carbon.hpp"
+
+struct LongRetardedJavaCancerFactory {
+    int   i;
+    float f;
+    CARBON_SERIALIZABLE(LongRetardedJavaCancerFactory, i, f)
+};
+
+int main()
+{
+}
+#endif
