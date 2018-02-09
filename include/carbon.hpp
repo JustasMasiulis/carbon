41fe762eb33355f39e87af1e175a07b6c583469e
Justas Masiulis <justas.masiulis@yahoo.com>
2018-02-09 23:30:22 +0200

    started rewrite

--- include/carbon.hpp
+++ include/carbon.hpp
@@ -16,11 +16,8 @@
 
 #pragma once
 #include "carbon/detail/generated_macros.hpp"
-#include "carbon/detail/nargs_macro.hpp"
-#include "carbon/serializer.hpp"
-#include "carbon/proxies.hpp"
-#include "carbon/detail/binary_archive.hpp"
-#include "carbon/detail/tstring.hpp"
+#include "carbon/detail/util_macros.hpp"
+#include <utility>
 
 // needed to fix msvc va args expansion
 #define CRBN_DETAIL_EXPAND(...) __VA_ARGS__
@@ -31,10 +28,26 @@
 #define CARBON_NAMED_SERIALIZABLE_(...) \
     CRBN_DETAIL_CAT(CRBN_DETAIL_NAMED_SERIALIZABLE_, CRBN_DETAIL_NARGS(__VA_ARGS__))
 
-#define CARBON_SERIALIZABLE(class_name, ...)    \
-    using serializer_type = CRBN_DETAIL_EXPAND( \
-        CARBON_SERIALIZABLE_(__VA_ARGS__)(class_name, __VA_ARGS__))
+#define CARBON_SERIALIZABLE(class_name, ...) \
+    CRBN_DETAIL_EXPAND(CARBON_SERIALIZABLE_(__VA_ARGS__)(class_name, __VA_ARGS__))
 
 #define CARBON_NAMED_SERIALIZABLE(class_name, ...) \
-    using serializer_type = CRBN_DETAIL_EXPAND(    \
+    CRBN_DETAIL_EXPAND(                            \
         CARBON_NAMED_SERIALIZABLE_(__VA_ARGS__)(class_name, __VA_ARGS__))
+
+namespace carbon {
+
+    template<class T, class Archive, class... Args>
+    inline T construct(Archive& archive, Args&&... args)
+    {
+        using carbon_type = typename T::template carbon_type<const T, Archive&>;
+
+        typename std::aligned_storage<sizeof(T), alignof(T)>::type storage;
+        ::new (static_cast<void*>(&storage))
+            T(carbon_type{ reinterpret_cast<const T*>(&storage), archive },
+              std::forward<Args>(args)...);
+
+        return *reinterpret_cast<T*>(&storage);
+    }
+
+} // namespace carbon
--- test/main.cpp
+++ test/main.cpp
@@ -1,67 +1,80 @@
+//#include "../include/carbon.hpp"
+//#include <fstream>
+//#include <vector>
+//#include <list>
+//#include <tuple>
+//#include <array>
+//#include <map>
+//#include "../include/carbon/archives/json.hpp"
+//
+// struct baz {
+//    std::vector<int> v = { 5, 6, 3, 4, 5 };
+//    std::size_t      s = 2;
+//    CARBON_NAMED_SERIALIZABLE(baz, v, s);
+//};
+//
+// struct test {
+//    int   a = 78;
+//    float b = 79;
+//    CARBON_NAMED_SERIALIZABLE(test, a, b);
+//};
+//
+// struct triv_copyable {
+//    std::map<std::string, int>                    fuckmedaddy{ {"asdasd", 6} };
+//    int                                           i;
+//    std::tuple<baz, std::pair<test, test>, float> tup;
+//    float                                         floateroo;
+//    std::array<std::array<test, 2>, 2> arr{ std::array<test, 2>{ test{ 1, 2.f },
+//                                                                 test{ 2, 3.f } },
+//                                            { test{ 4, 5.f }, test{ 6, 7.f } } };
+//    double                             d;
+//
+//    CARBON_NAMED_SERIALIZABLE(triv_copyable, fuckmedaddy, i, tup, floateroo, arr,
+//    d);
+//};
+//
 #include "../include/carbon.hpp"
-#include <fstream>
-#include <vector>
-#include <list>
-#include <tuple>
-#include <array>
-#include <map>
-#include "../include/carbon/archives/json.hpp"
-
-struct baz {
-    std::vector<int> v = { 5, 6, 3, 4, 5 };
-    std::size_t      s = 2;
-    CARBON_NAMED_SERIALIZABLE(baz, v, s);
-};
-
-struct test {
-    int   a = 78;
-    float b = 79;
-    CARBON_NAMED_SERIALIZABLE(test, a, b);
-};
-
-struct triv_copyable {
-    std::map<std::string, int>                    fuckmedaddy{ {"asdasd", 6} };
-    int                                           i;
-    std::tuple<baz, std::pair<test, test>, float> tup;
-    float                                         floateroo;
-    std::array<std::array<test, 2>, 2> arr{ std::array<test, 2>{ test{ 1, 2.f },
-                                                                 test{ 2, 3.f } },
-                                            { test{ 4, 5.f }, test{ 6, 7.f } } };
-    double                             d;
-
-    CARBON_NAMED_SERIALIZABLE(triv_copyable, fuckmedaddy, i, tup, floateroo, arr, d);
-};
-
 struct foo {
-    triv_copyable          tc;
-    int                    i;
-    std::tuple<int, float> t;
-    std::list<baz>         b;
-    std::array<int, 5>     a;
-    CARBON_NAMED_SERIALIZABLE(foo, tc, i, a);
+    template<class Carbon>
+    foo(Carbon c, const char* test_arg)
+    {}
+    // triv_copyable          tc;
+    int   i;
+    float f;
+    bool  b;
+    char  c;
+
+    CARBON_SERIALIZABLE(foo, i, f, b, c);
+    // CARBON_NAMED_SERIALIZABLE(foo, tc, i, a);
 };
 
 int main()
 {
-    foo f;
-    f.i    = 2;
-    f.t    = std::make_tuple(2, 3.f);
-    f.b    = { { { 1, 2, 3 }, 4 }, { { 4, 5, 6, 7 }, 5 }, { { 8, 9 }, 10 } };
-    f.a[0] = 1;
-    f.a[1] = 2;
-
-    triv_copyable tc;
-    /*tc.d   = 1;
-    tc.floateroo   = 2;
-    tc.i   = 5;
-    tc.testerroo.a = 6;
-    tc.testerroo.b = 1233;*/
-    std::ifstream in("testj.txt", std::ios::binary);
-
-    /*if (in.is_open()) {
-        carbon::deserialize<carbon::archive::json_input>(tc, in);
-        in.close();
-    }*/
-    std::ofstream out("testj.txt", std::ios::binary);
-    carbon::serialize<carbon::archive::json_output>(tc, out);
+    float archive;
+    carbon::construct<foo>(archive, "wubadubdub");
 }
+//
+// int main()
+//{
+//    foo f;
+//    f.i    = 2;
+//    f.t    = std::make_tuple(2, 3.f);
+//    f.b    = { { { 1, 2, 3 }, 4 }, { { 4, 5, 6, 7 }, 5 }, { { 8, 9 }, 10 } };
+//    f.a[0] = 1;
+//    f.a[1] = 2;
+//
+//    triv_copyable tc;
+//    /*tc.d   = 1;
+//    tc.floateroo   = 2;
+//    tc.i   = 5;
+//    tc.testerroo.a = 6;
+//    tc.testerroo.b = 1233;*/
+//    std::ifstream in("testj.txt", std::ios::binary);
+//
+//    /*if (in.is_open()) {
+//        carbon::deserialize<carbon::archive::json_input>(tc, in);
+//        in.close();
+//    }*/
+//    std::ofstream out("testj.txt", std::ios::binary);
+//    carbon::serialize<carbon::archive::json_output>(tc, out);
+//}
