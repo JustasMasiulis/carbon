882c98d89d61177b85ab25a032c8da5f92628060
Justas Masiulis <justas.masiulis@yahoo.com>
2018-04-21 22:33:28 +0300

    finished adding tags

--- include/carbon/detail/tags.hpp
+++ include/carbon/detail/tags.hpp
@@ -1,7 +1,8 @@
 #ifndef CARBON_DETAIL_TAGS_HPP
 #define CARBON_DETAIL_TAGS_HPP
 
-#include <type_traits>
+#include "traits.hpp"
+
 #include <iterator>
 #include <utility>
 
@@ -31,6 +32,23 @@ namespace carbon { namespace detail {
     template<class T, std::size_t N>
     struct is_array<T[N]> : std::true_type {};
 
+
+    template<class T, std::size_t N>
+    struct array_traits;
+
+    template<class T, std::size_t N>
+    struct array_traits<std::array<T, N>> {
+        using value_type                  = T;
+        constexpr static std::size_t size = N;
+    };
+
+    template<class T, std::size_t N>
+    struct array_traits<T[N]> {
+        using value_type                  = T;
+        constexpr static std::size_t size = N;
+    };
+
+
     template<class T>
     struct is_continguous : std::false_type {};
 
@@ -40,12 +58,21 @@ namespace carbon { namespace detail {
     template<class T, class Alloc>
     struct is_continguous<std::vector<T, Alloc>> : std::true_type {};
 
+
+    template<class T, class = void>
+    struct is_tuple_like : std::false_type {};
+
+    template<class T>
+    struct is_tuple_like<T, std::void_t<std::tuple_size<T>>> : std::true_type {};
+
     namespace tag {
 
+        struct specialized {};
         struct trivially_copyable {};
         struct array {};
         struct iterable {};
         struct continguos_iterable {};
+        struct tuple_tag {};
         struct none {};
 
     } // namespace tag
@@ -53,6 +80,8 @@ namespace carbon { namespace detail {
     template<class T>
     inline constexpr auto serialization_tag() noexcept
     {
+        if constexpr (traits::has_carbon_type<T>::value)
+            return tag::specialized();
         if constexpr (std::is_trivially_copyable_v<T>)
             return tag::trivially_copyable();
         else if constexpr (is_iterable<T>::value) {
@@ -63,7 +92,10 @@ namespace carbon { namespace detail {
             else
                 return tag::iterable();
         }
-        else if constexpr ()
+        else if constexpr (is_tuple_like<T>::value)
+            return tag::tuple_tag();
+        else
+            return tag::none();
     }
 
 }} // namespace carbon::detail
