41c6fc9c44477c97c46ec20e1b1d8c2eaf5d27f0
JustasMasiulis <justas.masiulis@yahoo.com>
2018-05-18 17:10:02 +0300

    made tests compile once again

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,12 @@
-#include "../include/carbon/unchecked_memory_archive.hpp"
+#define CARBON_IO_TRACK_BYTES_COPIED
+#include "../include/carbon/io/unchecked_memory.hpp"
+#include "../include/carbon/format/binary.hpp"
 #include "../include/carbon.hpp"
 #include <iostream>
 #include <fstream>
 #include <deque>
+#include <vector>
+#include <istream>
 
 struct bar {
     int                  i;
@@ -18,22 +22,23 @@ struct foo {
 
 int main()
 {
+    using serializer =
+        carbon::serializer_t<carbon::format::binary, carbon::io::unchecked_memory>;
+
     char          buffer[0x1000];
     foo           f;
     std::ifstream in("test.txt", std::ios::binary);
     if(in.is_open()) {
         std::copy(std::istream_iterator<char>(in), {}, buffer);
-        carbon::input_archive::unchecked_memory archive(buffer);
-        archive(f);
+        carbon::load<serializer>(f, buffer);
 
         std::cout << f.b.i << f.b.f << f.s[0] << f.s[1] << f.s[2] << f.d;
         in.close();
     }
 
-    f = foo{ { 2, 6.f }, { 'b', 'c', 'd' }, { 2.0 } };
-    carbon::output_archive::unchecked_memory archive(buffer);
-    archive(f);
+    f           = foo{ { 2, 6.f }, { 'b', 'c', 'd' }, { 2.0 } };
+    auto format = carbon::save<serializer>(f, buffer);
 
     std::ofstream out("test.txt", std::ios::binary);
-    std::copy(buffer, buffer + archive.copied(), std::ostream_iterator<char>(out));
+    std::copy(buffer, buffer + format.io().copied(), std::ostream_iterator<char>(out));
 }
