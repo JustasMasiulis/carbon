0353f8a54f1d10eed10c199c0f1154f8f2605f59
JustasMasiulis <justas.masiulis@yahoo.com>
2018-05-18 17:09:27 +0300

    removed some unused stuff from config, added more options

--- include/carbon/detail/config.hpp
+++ include/carbon/detail/config.hpp
@@ -1,14 +1,21 @@
 #ifndef CARBON_DETAIL_CONFIG_HPP
 #define CARBON_DETAIL_CONFIG_HPP
 
-#ifdef CARBON_IO_TRACK_BYTES_COPIED
-#define CARBON_IO_REF_IF_BYTE_COUNT_TRACKED(x) x&
-#else
-#define CARBON_IO_REF_IF_BYTE_COUNT_TRACKED(x) x
-#endif
-
 #ifndef CARBON_IO_MEMORY_OUT_OF_RANGE
 #define CARBON_IO_MEMORY_OUT_OF_RANGE(str) throw std::out_of_range(str)
+#endif
+
+#ifndef CARBON_TYPE_NAME
+#define CARBON_TYPE_NAME carbon_type
+#endif
+
+#ifndef CARBON_CONTAINER_SIZE_TYPE
+#define CARBON_CONTAINER_SIZE_TYPE std::uint32_t
+#endif
+
+#ifndef CARBON_CONTAINER_SIZE_OVERFLOW
+#define CARBON_CONTAINER_SIZE_OVERFLOW \
+    throw std::length_error("carbon iterable container size too long")
 #endif
 
 #endif
